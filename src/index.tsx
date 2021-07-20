e4d3b2d0c09e9d0e059256faea2d8789b934fa71
Marlou Lopez <08.marlou.lopez@gmail.com>
2021-07-20 15:39:27 +0800

    remove msw related files

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-// // Start the mocking conditionally.
-// if (process.env.NODE_ENV === 'development') {
-//   // eslint-disable-next-line global-require
-//   const { worker } = require('./mocks/browser');
-//   worker.start();
-// }
 
 ReactDOM.render(
   <React.StrictMode>
